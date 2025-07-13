ce5774f298c5dd8c8267397148dca17fc2e75cb9
ryanbai519 <bair6774@my.sduhsd.net>
2025-07-13 11:14:30 -0700

    update TOM_utils, tom matrix values are stored on our shared drive as tom_matrix_values.csv.
    updated TOM_utils to filter out unimportant genes, as the tom_matrix_values was ~5gb

--- TOM_utils.py
+++ TOM_utils.py
@@ -1,40 +1,37 @@
-import numpy as np
-
-def compute_tom(transformed_data, beta=6):
-    pearson_correlation_matrix = pearson_correlation(transformed_data)
-    adjacency_matrix = adjacency(pearson_correlation_matrix, beta)
-    connectivity = np.sum(adjacency_matrix, axis=1)
-    shared_neighbors = adjacency_matrix @ adjacency_matrix
-    num_genes = adjacency_matrix.shape[0]
-    TOM_matrix = np.zeros_like(adjacency_matrix)
-    min_connectivity = np.minimum.outer(connectivity, connectivity)
-    for i in range(num_genes):
-        for j in range(num_genes):
-            if i == j:
-                TOM_matrix[i][j] = 1
-            else:
-                numerator = adjacency_matrix[i, j] + shared_neighbors[i, j]   
-                denominator = 1 - adjacency_matrix[i, j] + min_connectivity[i, j]
-                if denominator == 0:
-                    TOM_matrix[i][j] = 0
-                else:
-                    TOM_matrix[i][j] = numerator / denominator
-    return TOM_matrix
-
-def pearson_correlation(transformed_data):
-    pearson_correlation_matrix = transformed_data @ transformed_data.T
-    return pearson_correlation_matrix
-
-def adjacency(pearson_correlation_matrix, beta):
-    return np.abs(pearson_correlation_matrix) ** beta
-
-
-
-#calculating the actual TOM matrix, only need to do once as we will save the matrix in a file
 import pandas as pd
-transformed_data = pd.read_csv('data/normalized_counts.csv')
-gene_names = transformed_data.iloc[:, 0]
-transformed_data = transformed_data.iloc[:, 1:].to_numpy()
-TOM_matrix = compute_tom(transformed_data)
-tom_data_frame = pd.DataFrame(TOM_matrix, columns=gene_names, index=gene_names)
-tom_data_frame.to_csv('tom_matrix_values.csv')
+import numpy as np
+from sklearn.feature_selection import VarianceThreshold
+
+data = pd.read_csv('data/normalized_counts.csv', index_col=0)
+data = data.iloc[:-4]
+data = data.T
+
+selector = VarianceThreshold(threshold=0.37)
+X_highvar = selector.fit_transform(data)
+selected_genes = data.columns[selector.get_support()]
+X_df = pd.DataFrame(X_highvar, index=data.index, columns=selected_genes)
+
+def pearson_correlation(data):
+    standardized = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
+    return np.corrcoef(standardized, rowvar=False)
+
+def adjacency(cor_matrix, beta):
+    return np.abs(cor_matrix) ** beta
+
+def compute_tom(adjacency_matrix):
+    k = np.sum(adjacency_matrix, axis=1)
+    num_genes = adjacency_matrix.shape[0]
+    shared = adjacency_matrix @ adjacency_matrix
+    min_k = np.minimum.outer(k, k)
+    np.fill_diagonal(min_k, k)
+    denom = min_k - adjacency_matrix + 1e-10
+    tom = (shared + adjacency_matrix) / denom
+    np.fill_diagonal(tom, 1.0)
+    return tom
+
+cor = pearson_correlation(X_df.to_numpy())
+adj = adjacency(cor, beta=6)
+tom = compute_tom(adj)
+
+tom_df = pd.DataFrame(tom, index=selected_genes, columns=selected_genes)
+tom_df.to_csv("tom_matrix_values.csv")
